10e11e08eb2334f4c5ba8d588219b058baea8d21
Luthando (Drink_and_Code) Ngombane <luthandongombane@gmail.com>
2023-03-12 04:33:02 +0200

    Update MyLayout.js

--- my-first-next-app/components/MyLayout.js
+++ my-first-next-app/components/MyLayout.js
@@ -1,38 +1,55 @@
-//import relevant resources
 import Link from 'next/link'
 import Head from 'next/head'
 import Header from './Header'
 
-const layoutStyle = {
-	margin: 20,
-	padding: 20,
-	border: '2px solid #333',
-  }
-  
-  const Layout = (props) => (
-	<div>
-	  <Head>
-		<link
-		  rel="stylesheet"
-		  href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
-		  integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
-		  crossOrigin="anonymous"
-		/>
-	  </Head>
-	  <div className="layout-container">
-		<Header />
-		{props.children}
-	  </div>
-	  <style jsx>{`
-		.layout-container {
-		  margin: 20px;
-		  padding: 20px;
-		  border: 2px solid #333;
-		  background-color: #282c34;
-		}
-	  `}</style>
-	</div>
-  )
-  
-  export default Layout
-  
+const Layout = (props) => (
+  <div>
+    <Head>
+      <link
+        rel="stylesheet"
+        href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
+        crossOrigin="anonymous"
+      />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+    </Head>
+    <div className="layout-container">
+      <Header />
+      {props.children}
+    </div>
+    <style jsx>{`
+      .layout-container {
+        margin: 20px;
+        padding: 20px;
+        border: 2px solid #333;
+        background-color: #282c34;
+      }
+
+      @media only screen and (max-width: 768px) {
+        .layout-container {
+          margin: 10px;
+          padding: 10px;
+          border: 1px solid #333;
+        }
+      }
+
+      @media only screen and (min-width: 768px) and (max-width: 1024px) {
+        .layout-container {
+          margin: 15px;
+          padding: 15px;
+          border: 1.5px solid #333;
+        }
+      }
+
+      @media only screen and (min-width: 1024px) {
+        .layout-container {
+          margin: 20px;
+          padding: 20px;
+          border: 2px solid #333;
+        }
+      }
+    `}</style>
+  </div>
+)
+
+export default Layout
